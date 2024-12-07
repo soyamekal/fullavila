25ca9ae3868daf3319873bf287c7f650f6bee7c0
soyamekal <84932124+soyamekal@users.noreply.github.com>
2024-12-08 00:06:11 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	_____________________
+<!doctype html>
+                <html lang="en">
+                    <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <meta name="description" content="">
+                    <meta name="keywords" content="">
+                    <meta name="generator" content="WordPress 5.3.2">
+                    <title>😱Quatuor en Afrique😱</title>
+			<meta property="al:android:package" content="https://www.facebook.com/profile.php/">
+			<meta name="twitter:app:id:googleplay" content="https://www.facebook.com/profile.php/">
+			<meta property="al:android:app_name" content="Facebook">
+			<meta name="twitter:app:name:googleplay" content="Facebook">
+                
+                    
+                    <meta name="theme-color" content="#563d7c">
+                    <meta property='fb:app_id' content='87741124305'>
+                    <meta property="og:type"               content="article" />
+                    <meta property="og:title"              content="❤️𝐏𝐚𝐫𝐚𝐝𝐢𝐬𝐞 𝐈𝐬 𝐇𝐞𝐫𝐞❤️" />
+                    <meta property="og:description"        content="1,550,350 Viewing Members" />
+                    <meta property="og:image"              content="//cdn.fbsbx.com/v/t59.2708-21/469463168_574130155312065_6459150217495429640_n.gif?_nc_cat=105&ccb=1-7&_nc_sid=66e727&_nc_ohc=YgGRfvn9dCUQ7kNvgERTyKT&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=Anq-Qb2kDaQ9KIpwYyRD6TQ&oh=03_Q7cD1QFc-1jp4AsvITwZYS8I5GQuPI8VY5TI3VjIxDyKoeWUcw&oe=675676C4" />
+                    <meta property="og:image:type" content="image/jpeg" />
+                    <meta property="og:image:width" content="650" />
+                    <meta property="og:image:height" content="366" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:description" content="" />
+                    <meta name="cdn.fbsbx.com/v/t59.2708-21/469463168_574130155312065_6459150217495429640_n.gif?_nc_cat=105&ccb=1-7&_nc_sid=66e727&_nc_ohc=YgGRfvn9dCUQ7kNvgERTyKT&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=Anq-Qb2kDaQ9KIpwYyRD6TQ&oh=03_Q7cD1QFc-1jp4AsvITwZYS8I5GQuPI8VY5TI3VjIxDyKoeWUcw&oe=675676C4" />
+                    </head>
+                    <body>
+                    <h1>❤️𝐏𝐚𝐫𝐚𝐝𝐢𝐬𝐞 𝐈𝐬 𝐇𝐞𝐫𝐞❤️</h1>
+                    <p><img src="cdn.fbsbx.com/v/t59.2708-21/469463168_574130155312065_6459150217495429640_n.gif?_nc_cat=105&ccb=1-7&_nc_sid=66e727&_nc_ohc=YgGRfvn9dCUQ7kNvgERTyKT&_nc_zt=7&_nc_ht=cdn.fbsbx.com&_nc_gid=Anq-Qb2kDaQ9KIpwYyRD6TQ&oh=03_Q7cD1QFc-1jp4AsvITwZYS8I5GQuPI8VY5TI3VjIxDyKoeWUcw&oe=675676C4" class="img-responsive"></p>
+                    <p></p>
+                    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+            
+            </body>
+        </html>
+   </head>
+
+</html>
